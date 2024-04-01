437b915f5cb6f0ee200f6bc127fa9499e4e89728
hdbhandari <himanshubhandari5@yahoo.co.in>
2024-04-01 16:22:40 +0530

    converting class component to functional

--- react/src/Pages/App.js
+++ react/src/Pages/App.js
@@ -4,21 +4,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./Home"
 import DemoPage from "./DemoPage"
 
-class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/nav-demo" element={<DemoPage />} />
-          {/* To catch all the Routes that are not resolved */}
-          <Route path="*" element={<p>Path not resolved</p>} />
-        </Routes>
-      </Router>
-    )
-  }
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/nav-demo" element={<DemoPage />} />
+        {/* To catch all the Routes that are not resolved */}
+        <Route path="*" element={<p>Path not resolved</p>} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
